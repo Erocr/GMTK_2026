a6dddb12d0023bb906678c222ac9e67e70ac72eb
Elise <elise.ordoquy@icloud.com>
2026-07-23 18:45:12 +0200

    début du dict

--- Model.py
+++ Model.py
@@ -1,3 +1,4 @@
+from itertools import combinations
 class Model:
     def __init__(self):
         self.SCREEN_LENGTH = 1280
@@ -9,3 +10,10 @@ class Model:
 
     def add_animal(self, animal):
         self.animals.append(animal)
+
+    def dna_edit():
+        col = ['R', 'B', 'V']
+        sequences = combinations(col, 8)
+        dna_image = {}
+        for elt in sequences:
+            dna_image[elt] = None
