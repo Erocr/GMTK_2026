bc7f880e614a30f39011402a3a46d2595c73fdc5
Elise <elise.ordoquy@icloud.com>
2026-07-26 17:49:50 +0200

    je peux pas cliquer

--- Controller.py
+++ Controller.py
@@ -86,7 +86,7 @@ class Controller:
 
     def specie_chosen(self, mouse_pos:Vec):
         for spec in self.model.unlocked_species:
-            if self.opened_box and spec.pos.x < mouse_pos.x and spec.pos.x + 157 > mouse_pos.x and spec.pos.y < mouse_pos.y and spec.pos.y + 296> mouse_pos.y:
+            if self.opened_box and spec.pos.x < mouse_pos.x and spec.pos.x + self.model.ANIMAL_IN_GRAPH_SIZE.x > mouse_pos.x and spec.pos.y < mouse_pos.y and spec.pos.y + self.model.ANIMAL_IN_GRAPH_SIZE.y> mouse_pos.y:
                 self.opened_box.update_spec = spec
                 self.opened_box = None
                 self.model.score += 1
@@ -98,12 +98,14 @@ class Controller:
             self.view.resize(self.inputHandler.resized)
         
         if self.inputHandler.pressed("mouse_left"):
-            if not self.tree_window_clicked(self.inputHandler.mouse_pos):
-                self.search_animal(self.inputHandler.mouse_pos)
-            if self.tree_window.opened:
-                self.box_clicked(self.inputHandler.mouse_pos)
             if self.view.list_species_opened:
                 self.specie_chosen(self.inputHandler.mouse_pos)
+            elif self.tree_window.opened:
+                self.box_clicked(self.inputHandler.mouse_pos)
+            elif self.view.list_species_opened:
+                self.specie_chosen(self.inputHandler.mouse_pos)
+            elif not self.tree_window_clicked(self.inputHandler.mouse_pos):
+                self.search_animal(self.inputHandler.mouse_pos)
 
             for button in self.buttons_left:
                 button.is_clicked(self.inputHandler.mouse_pos)
