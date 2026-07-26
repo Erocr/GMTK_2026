9e66f1c5ff8fac81b2c1c7fef19f03f3d825f477
Elise <elise.ordoquy@icloud.com>
2026-07-26 12:19:20 +0200

    feat : draw box

--- Box.py
+++ Box.py
@@ -1,10 +1,22 @@
-
-
 from Vec import Vec
+from Specie import Specie
+from View import View
 
 
 class Box:
 
-    def __init__(self, pos:Vec, rad):
+    def __init__(self, pos:Vec, rad: float, view : View):
         self.pos = pos
         self.red = rad
+        self.view = view
+        self.spec : Specie  = None
+
+    def update_spec(self, new_spec : Specie):
+        self.spec = new_spec
+
+    def draw(self):
+        if self.spec:
+            for body_part in self.spec:
+                dna_to_draw = self.spec.list_body_parts[body_part].active_sec
+                image = self.view.draw_image[dna_to_draw] + "_mini"
+                self.view.draw_image(image, self.pos)
